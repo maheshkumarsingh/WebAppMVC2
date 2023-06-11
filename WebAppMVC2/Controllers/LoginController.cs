15c1c9b1ad1c5a2cac0fa376bd28f3087f34bcda
MaheshSingh <mahesh.singh2@cognizant.com>
2023-06-11 18:29:48 +0530

    Add project files.

---
+++ WebAppMVC2/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+using WebAppMVC2.Models;
+
+namespace WebAppMVC2.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        LindaEntities db = new LindaEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(user objchk)
+        {
+            if (ModelState.IsValid)
+            {
+                using (LindaEntities db = new LindaEntities())
+                {
+                    var obj = db.users.Where(a => a.username.Equals(objchk.username) && a.password.Equals(objchk.password)).FirstOrDefault();
+
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.id.ToString();
+                        Session["username"] = obj.username.ToString();
+                        return RedirectToAction("Index", "Home");
+
+                    }
+
+                    else
+                    {
+                        ModelState.AddModelError("", "The username or password is incorrect");
+                    }
+
+
+                }
+            }
+            return View(objchk);
+        }
+    }
+}
+        
---
+++ WebAppMVC2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppMVC2.MvcApplication" Language="C#" %>
